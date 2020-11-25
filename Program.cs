7bbe3c96d2c7bcb4aa129f433a66d64182ea652a
Matthew Donoughe <matthew_donoughe@waters.com>
2020-11-25 09:46:49 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RefPropCrash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDelegate(() => new Model { Value = 1 });
+            // Compiler crashes when this next line is uncommented:
+            TestExpression(() => new Model { Value = 1 });
+        }
+
+        static void TestDelegate(Func<Model> expression)
+        {
+        }
+
+        static void TestExpression(Expression<Func<Model>> expression)
+        {
+        }
+    }
+
+    class Model
+    {
+        int value;
+        public ref int Value => ref value;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Ref Prop Crash
+
+This is a small repro for a compiler crash that occurs when asking the compiler to construct an expression tree that sets a ref property.
+
+Run `dotnet build` to crash.
